c5b8a577f69304d254d84606debb8cbc2ad09c14
Jalyna <jalyna.schroeder@gmail.com>
2017-06-24 16:57:32 +0200

    init repo

---
+++ quiz.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  var answers = [];
+
+  var redirectToResult = function() {
+    var points = 0;
+    for(var i = 0; i < quiz.questions.length; i++) {
+      var question = quiz.questions[i];
+      if((question.answer - 1) === answers[i]) {
+        points++;
+      }
+    }
+    alert(points);
+    $('#quiz').html('');
+  }
+
+  var drawQuestion = function() {
+    if(answers.length >= quiz.questions.length) {
+      redirectToResult();
+      return;
+    }
+    var question = quiz.questions[answers.length];
+    var answerHtml = '';
+    for(var i = 0; i < question.answers.length; i++) {
+      answer = question.answers[i];
+      answerHtml = answerHtml + `
+        <div class="col-sm-3">
+          <a href="#" class="btn btn-block btn-default btn-lg js-answer"
+          data-answer="${i}">
+            ${answer.text}
+          </a>
+        </div>
+      `;
+    }
+    var html = `
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">${question.question}</h3>
+        </div>
+        <div class="panel-body">
+          <div class="row">
+            ${answerHtml}
+          </div>
+        </div>
+      </div>
+    `;
+    $('#quiz').html(html);
+  };
+
+  $('body').on('click', '.js-answer', function(e) {
+    e.preventDefault();
+    var answer = parseInt($(this).data('answer'));
+    answers.push(answer);
+    drawQuestion();
+  });
+
+  drawQuestion();
+});
