b87167b9c81f754b9584a59dfca1c62e6c3159c8
Jalyna <jalyna.schroeder@gmail.com>
2017-07-06 21:32:41 +0200

    extract quiz to js file

---
+++ assets/quizzes/ernaehrung.js
@@ -0,0 +1,93 @@
+window.afterQuiz = {
+  0: '/quizzes/ergebnisse/ernaehrung_karotte.html',
+  3: '/quizzes/ergebnisse/ernaehrung_karotte.html',
+  6: '/quizzes/ergebnisse/ernaehrung_karotte.html'
+}
+window.quiz = {
+  questions: [
+    {
+      question: "Was ist geeignet zum Essen für das Kaninchen?",
+      answer: 2,
+      answers: [
+        {
+          text: "Schafgarbe"
+        },
+        {
+          text: "Rollis"
+        }
+      ]
+    },
+    {
+      question: "Sollten Kaninchen Wegeriche essen?",
+      answer: 1,
+      answers: [
+        {
+          text: "Ja"
+        },
+        {
+          text: "Nein"
+        }
+      ]
+    },
+    {
+      question: "Sollten Kaninchen Luzerne essen?",
+      answer: 1,
+      answers: [
+        {
+          text: "Ja"
+        },
+        {
+          text: "Nein"
+        }
+      ]
+    },
+    {
+      question: "Sollten Kaninchen Sonnenblume essen?",
+      answer: 1,
+      answers: [
+        {
+          text: "Ja"
+        },
+        {
+          text: "Nein"
+        }
+      ]
+    },
+    {
+      question: "Sollten Kaninchen Hollunder essen?",
+      answer: 2,
+      answers: [
+        {
+          text: "Ja"
+        },
+        {
+          text: "Nein"
+        }
+      ]
+    },
+    {
+      question: "Sollten Kaninchen Avocado essen?",
+      answer: 2,
+      answers: [
+        {
+          text: "Ja"
+        },
+        {
+          text: "Nein"
+        }
+      ]
+    },
+    {
+      question: "Sollten Kaninchen Chinakohl essen?",
+      answer: 1,
+      answers: [
+        {
+          text: "Ja"
+        },
+        {
+          text: "Nein"
+        }
+      ]
+    }
+  ]
+}
