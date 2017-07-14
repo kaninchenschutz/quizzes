d64697f2b6abd5120f8d7d11238a8918669d340d
Jalyna <jalyna.schroeder@gmail.com>
2017-07-14 17:55:19 +0200

    fix es issues

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,8 +1,10 @@
-window.calculatePoints = function(an) {
+'use strict';
+
+window.calculatePoints = function (an) {
   var points = 0;
-  for(var i = 0; i < quiz.questions.length; i++) {
+  for (var i = 0; i < quiz.questions.length; i++) {
     var question = quiz.questions[i];
-    if((question.answer - 1) === an[i]) {
+    if (question.answer - 1 === an[i]) {
       points++;
     }
   }
@@ -10,13 +12,13 @@ window.calculatePoints = function(an) {
   return points;
 };
 
-window.qq = function(p) {
+window.qq = function (p) {
   var sPageURL = window.location.search.substring(1);
   var sURLVariables = sPageURL.split('&');
   var arr = [];
-  for(var i = 0; i < sURLVariables.length; i++) {
+  for (var i = 0; i < sURLVariables.length; i++) {
     var sParameterName = sURLVariables[i].split('=');
-    if(sParameterName[0] == p) {
+    if (sParameterName[0] == p) {
       arr.push(parseInt(sParameterName[1]));
     }
   }
@@ -24,130 +26,65 @@ window.qq = function(p) {
   return arr;
 };
 
-window.drawAnswers = function() {
+window.drawAnswers = function () {
   var answerHtml = '';
   var answers = qq('answers[]');
-  for(var i = 0; i < quiz.questions.length; i++) {
+  for (var i = 0; i < quiz.questions.length; i++) {
     var question = quiz.questions[i];
     var questionImg = '';
     var answerImg = '';
     var htmlClass = '';
-    if(question.image) {
-      questionImg = `
-        <div style="height:90px;width:90px;background-image:url(${question.image});background-repeat:no-repeat; background-position: center; background-size:cover;">
-        </div>
-      `;
+    if (question.image) {
+      questionImg = '\n        <div style="height:90px;width:90px;background-image:url(' + question.image + ');background-repeat:no-repeat; background-position: center; background-size:cover;">\n        </div>\n      ';
     }
-    if(question.answers[question.answer-1].image) {
-      questionImg = `
-        <div style="height:90px;width:90px;background-image:url(${question.answers[question.answer-1].image});background-repeat:no-repeat; background-position: center; background-size:cover;">
-        </div>
-      `;
+    if (question.answers[question.answer - 1].image) {
+      questionImg = '\n        <div style="height:90px;width:90px;background-image:url(' + question.answers[question.answer - 1].image + ');background-repeat:no-repeat; background-position: center; background-size:cover;">\n        </div>\n      ';
     }
-    if(answers && (question.answer - 1) == answers[i]) {
+    if (answers && question.answer - 1 == answers[i]) {
       htmlClass = 'list-group-item-success';
-    } else if(answers && answers.length > 0) {
+    } else if (answers && answers.length > 0) {
       htmlClass = 'list-group-item-danger';
     }
-    answerHtml = answerHtml + `
-      <li class="list-group-item ${htmlClass}">
-        <div class="row">
-          <div class="col-sm-6">
-            <strong>${question.question}</strong>
-            ${questionImg}
-          </div>
-          <div class="col-sm-6 text-right">
-            ${question.answers[question.answer-1].text}
-            ${question.answerText ? '<div class="small">' + question.answerText + '</div>' : ''}
-            ${answerImg}
-          </div>
-        </div>
-        <div class="clearfix"></div>
-      </li>
-    `;
+    answerHtml = answerHtml + ('\n      <li class="list-group-item ' + htmlClass + '">\n        <div class="row">\n          <div class="col-sm-6">\n            <strong>' + question.question + '</strong>\n            ' + questionImg + '\n          </div>\n          <div class="col-sm-6 text-right">\n            ' + question.answers[question.answer - 1].text + '\n            ' + (question.answerText ? '<div class="small">' + question.answerText + '</div>' : '') + '\n            ' + answerImg + '\n          </div>\n        </div>\n        <div class="clearfix"></div>\n      </li>\n    ');
   }
 
-  var html = `
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3 class="panel-title">
-          Auflösung
-        </h3>
-      </div>
-      <ul class="list-group">
-        ${answerHtml}
-      </ul>
-    </div>
-  `;
+  var html = '\n    <div class="panel panel-default">\n      <div class="panel-heading">\n        <h3 class="panel-title">\n          Aufl\xF6sung\n        </h3>\n      </div>\n      <ul class="list-group">\n        ' + answerHtml + '\n      </ul>\n    </div>\n  ';
   $('#answers').html(html);
 
-  if(answers) {
+  if (answers) {
     $('#points').html('<br />' + calculatePoints(answers) + ' / ' + quiz.questions.length + ' Punkten');
   }
 };
 
-window.drawQuestion = function() {
-  if(answers.length >= quiz.questions.length) {
+window.drawQuestion = function () {
+  if (answers.length >= quiz.questions.length) {
     redirectToResult();
     return;
   }
   var question = quiz.questions[answers.length];
   var answerHtml = '';
-  for(var i = 0; i < question.answers.length; i++) {
+  for (var i = 0; i < question.answers.length; i++) {
     answer = question.answers[i];
     var answerImage = '';
-    if(answer.image) {
-      answerImage = `
-        <div style="height:180px;background-image:url(${answer.image});background-repeat:no-repeat; background-position: center; background-size:cover;">
-        </div>
-      `;
+    if (answer.image) {
+      answerImage = '\n        <div style="height:180px;background-image:url(' + answer.image + ');background-repeat:no-repeat; background-position: center; background-size:cover;">\n        </div>\n      ';
     }
-    answerHtml = answerHtml + `
-      <div class="col-sm-6" style="margin-bottom:5px;">
-        <a href="#" class="btn btn-block btn-default btn-lg js-answer"
-        data-answer="${i}">
-          ${answerImage}
-          ${answer.text}
-        </a>
-      </div>
-    `;
+    answerHtml = answerHtml + ('\n      <div class="col-sm-6" style="margin-bottom:5px;">\n        <a href="#" class="btn btn-block btn-default btn-lg js-answer"\n        data-answer="' + i + '">\n          ' + answerImage + '\n          ' + answer.text + '\n        </a>\n      </div>\n    ');
   }
   var questionImage = '';
-  if(question.image) {
-    questionImage = `
-      <div class="text-center">
-        <img src="${question.image}" class="img-responsive" style="max-height:230px;margin:0px auto;" />
-      </div>
-      <hr />
-    `;
+  if (question.image) {
+    questionImage = '\n      <div class="text-center">\n        <img src="' + question.image + '" class="img-responsive" style="max-height:230px;margin:0px auto;" />\n      </div>\n      <hr />\n    ';
   }
 
-  var html = `
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h3 class="panel-title">
-          ${question.question}
-          <div class="badge pull-left">
-            ${answers.length+1}/${quiz.questions.length}
-          </div>
-        </h3>
-      </div>
-      <div class="panel-body">
-        ${questionImage}
-        <div class="row">
-          ${answerHtml}
-        </div>
-      </div>
-    </div>
-  `;
+  var html = '\n    <div class="panel panel-default">\n      <div class="panel-heading">\n        <h3 class="panel-title">\n          ' + question.question + '\n          <div class="badge pull-left">\n            ' + (answers.length + 1) + '/' + quiz.questions.length + '\n          </div>\n        </h3>\n      </div>\n      <div class="panel-body">\n        ' + questionImage + '\n        <div class="row">\n          ' + answerHtml + '\n        </div>\n      </div>\n    </div>\n  ';
   $('#quiz').html(html);
 };
 
-window.redirectToResult = function() {
+window.redirectToResult = function () {
   var points = calculatePoints(answers);
   var url = afterQuiz[0];
-  for(var i in afterQuiz) {
-    if(points >= i) {
+  for (var i in afterQuiz) {
+    if (points >= i) {
       url = afterQuiz[i];
     }
   }
@@ -157,12 +94,12 @@ window.redirectToResult = function() {
 
 var answers = [];
 
-$(document).ready(function() {
-  if(!$('#quiz').length) {
+$(document).ready(function () {
+  if (!$('#quiz').length) {
     return;
   }
 
-  $('body').on('click', '.js-answer', function(e) {
+  $('body').on('click', '.js-answer', function (e) {
     e.preventDefault();
     var answer = parseInt($(this).data('answer'));
     answers.push(answer);
@@ -172,8 +109,8 @@ $(document).ready(function() {
   drawQuestion();
 });
 
-$(document).ready(function() {
-  if(!$('#answers').length) {
+$(document).ready(function () {
+  if (!$('#answers').length) {
     return;
   }
 
